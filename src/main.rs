ac89ece8cd6e393c0135ec1711604397d6fd3676
Scott Loose <scotlandcloose@gmail.com>
2023-12-12 21:00:05 -0600

    progress

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,32 @@ use std::io::{self, BufRead};
 fn main() {
     let input: String = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
     let mut ch_iter = input.chars();
+    let mut is_ansi_escape: bool = false;
+    let mut command: String = String::new();
     while let Some(ch) = ch_iter.next() {
-        if ch as u32 == 27 {
-            let mut sequence: String = String::new();
-            if let Some(ch) = ch_iter.next() {
-                if ch == '[' {
-                   sequence.push(ch);
-                   while let Some(ch) = ch_iter.next() {
-                        
-                   }
+        
+        if is_ansi_escape {
+            if ch == '[' || ch == '?' || ch == ';' || ch == '{' || ch == '}' || (ch >= '0' && ch <= '9') {
+                command.push(ch.clone());
+            } else if ch.is_alphabetic() {
+                command.push(ch.clone());
+                is_ansi_escape = false;
+                match command.chars().last().unwrap() {
+                    'm' => (),
+                    'h' => print!("\n\nHome: {}\n", command),
+                    'f' => print!("\n\nMoveTo: {}\n", command),
+                    none => print!("Command: {}, ", none),
                 }
+            } else {
+                print!("\nCommand: {}\n", command);
+                print!("'{}' is not a valid character for ansi escape sequence.", ch);
+                is_ansi_escape = false;
             }
+        }
+
+        if ch as u32 == 27 {
+            is_ansi_escape = true;
+            command = String::new();
         }
     }
 }
