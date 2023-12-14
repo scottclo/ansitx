196e9027e39d13e468cbad3aaab2c298c1a97456
Scott Loose <scotlandcloose@gmail.com>
2023-12-13 21:44:05 -0600

    added Vec<Vec<char>> buffer to handle cursor movment

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,55 @@
-use std::io::{self, BufRead};
+use std::io;
+
+struct Vec2 {
+    x: usize,
+    y: usize,
+}
 
 fn main() {
-    let input: String = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
+    let input: String = io::read_to_string(io::stdin()).unwrap();
     let mut ch_iter = input.chars();
     let mut is_ansi_escape: bool = false;
     let mut command: String = String::new();
+    let mut cursor: Vec2 = Vec2{x:0, y:0};
+    let mut buffer: Vec<Vec<char>> = Vec::new();
+    print!("{}\n\n\n\n", input);
+    print!("{:?}\n\n\n\n", input);
+
     while let Some(ch) = ch_iter.next() {
-        
         if is_ansi_escape {
             if ch == '[' || ch == '?' || ch == ';' || ch == '{' || ch == '}' || (ch >= '0' && ch <= '9') {
                 command.push(ch.clone());
+                continue;
             } else if ch.is_alphabetic() {
                 command.push(ch.clone());
                 is_ansi_escape = false;
                 match command.chars().last().unwrap() {
-                    'm' => (),
-                    'h' => print!("\n\nHome: {}\n", command),
-                    'f' => print!("\n\nMoveTo: {}\n", command),
-                    none => print!("Command: {}, ", none),
+                    _none => (),
                 }
+                continue;
             } else {
-                print!("\nCommand: {}\n", command);
-                print!("'{}' is not a valid character for ansi escape sequence.", ch);
                 is_ansi_escape = false;
+                continue;
             }
         }
 
         if ch as u32 == 27 {
             is_ansi_escape = true;
             command = String::new();
+            continue;   
+        } else if ch == '\n' {
+            cursor.x = 0;
+            cursor.y += 1;
+            continue;
+        } else {
+            buffer.resize_with(cursor.y + 1, Default::default);
+            buffer[cursor.y].resize(cursor.x + 1, ' ');
+            buffer[cursor.y][cursor.x] = ch.clone();
+            cursor.x += 1;
         }
+    }
+
+    for line in buffer{
+        print!("{:?}\n", line);
     }
 }
