d2f69522bd401665d6226ac645bd7086eef774bf
Scott Loose <scotlandcloose@gmail.com>
2023-12-14 22:07:05 -0600

    It kind of works

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,104 @@
-use std::io;
+use std::{io, env};
 
-struct Vec2 {
+
+#[derive(Debug)]
+struct Cursor {
     x: usize,
     y: usize,
 }
 
+impl Cursor{
+    fn move_left(&mut self, x: usize) {
+        if x < self.x {
+            self.x -= x;
+        } else {
+            self.x = 0;
+        }
+    }
+    fn move_right(&mut self, x:usize) {
+        self.x += x;
+    }
+    fn move_up(&mut self, y:usize) {
+        if y < self.y {
+            self.y -= y;
+        } else {
+            self.y = 0;
+        }
+    }
+    fn move_down(&mut self, y:usize) {
+        self.y += y;
+    }
+}
+
+
+#[derive(Debug)]
+struct Command {
+    function: char,
+    args: Vec<String>,
+}
+
+impl Command {
+    fn new() -> Command{
+        Command{
+            function: char::default(),
+            args: Vec::new(),
+        }
+    }
+    fn get_arg_usize(&self, index: usize) -> usize {
+        self.args[index].parse::<usize>().unwrap()
+    }
+}
+
 fn main() {
+    env::set_var("RUST_BACKTRACE", "full");
     let input: String = io::read_to_string(io::stdin()).unwrap();
     let mut ch_iter = input.chars();
     let mut is_ansi_escape: bool = false;
-    let mut command: String = String::new();
-    let mut cursor: Vec2 = Vec2{x:0, y:0};
+    let mut command: Command = Command::new();
+    let mut cursor: Cursor = Cursor{x:0, y:0};
     let mut buffer: Vec<Vec<char>> = Vec::new();
-    print!("{}\n\n\n\n", input);
-    print!("{:?}\n\n\n\n", input);
-
+    //print!("{}\n\n", input);
     while let Some(ch) = ch_iter.next() {
         if is_ansi_escape {
-            if ch == '[' || ch == '?' || ch == ';' || ch == '{' || ch == '}' || (ch >= '0' && ch <= '9') {
-                command.push(ch.clone());
-                continue;
+            if ch == '[' || ch == ';' {
+                command.args.push(String::new());
+            } else if ch == '?' || (ch >= '0' && ch <= '9') {
+                let i = command.args.len() - 1;
+                command.args[i].push(ch.clone());
             } else if ch.is_alphabetic() {
-                command.push(ch.clone());
+                command.function = ch.clone();
                 is_ansi_escape = false;
-                match command.chars().last().unwrap() {
-                    _none => (),
+                match command.function {
+                    'A' => cursor.move_up(command.get_arg_usize(0)),
+                    'B' => cursor.move_down(command.get_arg_usize(0)),
+                    'C' => cursor.move_right(command.get_arg_usize(0)),
+                    'D' => cursor.move_left(command.get_arg_usize(0)),
+                    'm'|'l'|'h' => (),
+                    _none => print!("{:?}{}: Not Implimented\n", command.args, command.function),
                 }
-                continue;
             } else {
                 is_ansi_escape = false;
-                continue;
             }
+            continue;
         }
-
         if ch as u32 == 27 {
             is_ansi_escape = true;
-            command = String::new();
+            command = Command::new();
             continue;   
         } else if ch == '\n' {
             cursor.x = 0;
             cursor.y += 1;
             continue;
         } else {
-            buffer.resize_with(cursor.y + 1, Default::default);
+            if buffer.len() <= cursor.y {
+                buffer.resize_with(cursor.y + 1, Default::default);
+            }
             buffer[cursor.y].resize(cursor.x + 1, ' ');
             buffer[cursor.y][cursor.x] = ch.clone();
             cursor.x += 1;
         }
     }
-
     for line in buffer{
-        print!("{:?}\n", line);
+        print!("{}\n", line.iter().collect::<String>());
     }
 }
